bbecca64bbf8c40d6173b417942d933e25847f62
Jahyire Antunez <jahyireantunezsalazar@gmail.com>
2023-09-20 22:41:04 -0600

    Primer código completo

--- js/main.js
+++ js/main.js
@@ -1,63 +1,216 @@
+// Array de objetos
+let notasGuardadas = []
+
+// Captura body
+let body = document.getElementById("body")
+
+// Captura el inicio
+let inicio = document.getElementById("inicio")
+
+// Cpatura la seccion hoja para escribir
+let seccionCrearNotas = document.getElementById("crearNota")
+
+// Captura la seccion para ver las notas
+let seccionVerNotas = document.getElementById("verNota")
 // Captura el boton en inicio para comenzar
 let inicioBotonComenzar = document.getElementById("inicio__botonComenzar")
+inicioBotonComenzar.addEventListener("click", crearOVerNotas)
 
-// Captura la secccion principal llamada "inicio"
-let seccionInicio = document.getElementById("inicio")
+function crearOVerNotas() {
+    ocultarNodos(inicio)
+    body.innerHTML = `
+    <section id="crearOVerNotas">
+      <div id="crearOVerNotas__boton--regresar">
+        <button id="boton__regresar">Regresar</button>
+      </div>
+      <div id="crearOVerNotas__boton--crearVer">
+      <div id="boton__crear">
+        <button id="botonCrear">Crear nota</button>
+      </div>
+      <div id="boton__ver">
+        <button id="botonVer">Ver notas</button></div>
+      </div>
+    </section>
+    `
+    let botonRegresar = document.getElementById("boton__regresar")
+    botonRegresar.addEventListener("click", mostrarInicio)
+    let botonCrearNota = document.getElementById("botonCrear")
+    botonCrearNota.addEventListener("click", mostrarHoja)
+    let botonVerNotas = document.getElementById("botonVer")
+    botonVerNotas.addEventListener("click", verNotas)
+}
+function mostrarInicio() {
+    let seccionCrearOVerNotas = document.getElementById("crearOVerNotas")
+    ocultarNodos(seccionCrearOVerNotas)
+    body.innerHTML = `
+    <main id="inicio">
+        <h1 id="inicio__titulo">Deja de <span>procrastinar </span>y empieza a <span>crear</span>.</h1>
+        <button id="inicio__botonComenzar">Comenzar</button>
+    </main>
+    `
+    let inicioBotonComenzar = document.getElementById("inicio__botonComenzar")
+    inicioBotonComenzar.addEventListener("click", crearOVerNotas)
+}
+function mostrarHoja() {
+    let seccionCrearOVerNotas = document.getElementById("crearOVerNotas")
+    document.querySelector("#crearOVerNotas")
+    if (seccionCrearOVerNotas !== null) {
+        ocultarNodos(seccionCrearOVerNotas)
+    }
+    body.innerHTML = `
+    <section id="crearNota">
+      <div id="creaNota__hoja"><input type="text" name="tituloNota" id="tituloNota" placeholder="Titulo">
+        <textarea name="parrafoNota" id="parrafoNota" placeholder="Escribir..." cols="30" rows="10"></textarea>
+      </div>
+      <div id="crearNota__botones">
+        <button id="crearNota__botonRegresar">Regresar</button>
+        <button id="crearNota__botonGuardar">Guardar</button>
+      </div>
+    </section>
+    `
+    let botonRegresarAOpciones = document.getElementById("crearNota__botonRegresar")
+    botonRegresarAOpciones.addEventListener("click", crearOVerNotas)
+    let botonGuardarNota = document.getElementById("crearNota__botonGuardar")
+    botonGuardarNota.addEventListener("click", guardarNotas)
+}
+function guardarNotas() {
+    let entradaTituloNota = document.getElementById("tituloNota").value
+    let entradaParrafoNota = document.getElementById("parrafoNota").value
+    if (entradaTituloNota !== "" || entradaParrafoNota !== "") {
+        notasGuardadas.push({id: notasGuardadas.length, titulo: entradaTituloNota, parrafo: entradaParrafoNota})
+        localStorage.setItem("notas", JSON.stringify(notasGuardadas))
+        crearOVerNotas()
+        Swal.fire({
+            title: 'Guardado',
+            icon: 'success',
+            confirmButtonText: 'Continuar'
+        })
+    } else {
+        Swal.fire({
+            title: 'No ingresaste nada',
+            icon: 'warning',
+        })
+    }
+}
+function verNotas() {
+    let seccionCrearOVerNotas = document.getElementById("crearOVerNotas")
+    ocultarNodos(seccionCrearOVerNotas)
+    body.innerHTML = `
+    <section id="verNota">
+    </section>`
+    let seccionVerNotas = document.getElementById("verNota")
+    if (localStorage.getItem("notas")) {
+        notasGuardadas = JSON.parse(localStorage.getItem("notas"))
+        let contenidoNotas = ''
+        for (const nota of notasGuardadas) {
+            contenidoNotas += `
+                    <div class="nota" id=${nota.id}>
+                      <div class="contenido__nota">
+                        <h1>${nota.titulo}</h1>
+                        <p>${nota.parrafo}</p>
+                      </div>
+                    </div>
+                `
+        }
+        seccionVerNotas.innerHTML = `
+                <div id="verNotas--nav">
+                    <button class="boton" id="boton__regresar--verNota">Regresar</button>
+                    <input type="textarea" name="buscador" id="buscador" placeholder="Buscar">
+                    <button class="boton" id="boton__buscar">Buscar</button>
+                    <button class="boton" id="boton__filtar--A-Z">A-Z</button>
+                </div>
+                <div id="verNotas--contenido">
+                    ${contenidoNotas}
+                </div>
+                <div id="verNotas--crearNuevaNota">
+                    <button class="boton" id="boton__crear--verNota">Crear nota</button>
+                </div>
+            `
+    } else {
+        seccionVerNotas.innerHTML = `
+                <div id="verNotas--nav">
+                    <button class="boton" id="boton__regresar--verNota">Regresar</button>
+                    <input type="textarea" name="buscador" id="buscador" placeholder="Buscar">
+                    <button class="boton" id="boton__buscar">Buscar</button>
+                    <button class="boton" id="boton__filtar--A-Z">A-Z</button>
+                </div>
+                <div id="verNotas--contenidonull">
+                    <p>No hay nada por aquí :(</p>
+                </div>
+                <div id="verNotas--crearNuevaNota">
+                    <button class="boton" id="boton__crear--verNota">Crear nota</button>
+                </div>
+            `
+    }
+    let crearNotaEnSecVeNotas = document.getElementById("boton__crear--verNota")
+    crearNotaEnSecVeNotas.addEventListener("click", mostrarHoja)
+    let regresarEnSecVeNotas = document.getElementById("boton__regresar--verNota")
+    regresarEnSecVeNotas.addEventListener("click", crearOVerNotas)
+    let buscarNotas = document.getElementById("buscador")
+    let botonBuscarNotas = document.getElementById("boton__buscar")
+    botonBuscarNotas.addEventListener("click", function() {
+        buscador(notasGuardadas, buscarNotas.value)
+    })
+    let botonOrdenarAlfabeticamente = document.getElementById("boton__filtar--A-Z")
+    botonOrdenarAlfabeticamente.addEventListener("click", function() {
+        filtrarAlfabeticamente(notasGuardadas)
+    })
+}
+function buscador(notas, valorInput) {
+    valorInput = valorInput.toLowerCase()
+    let notasFiltradas = notas.filter((nota) => {
+        const titulo = nota.titulo.toLowerCase()
+        const parrafo = nota.parrafo.toLowerCase()
+        return titulo.includes(valorInput) || parrafo.includes(valorInput)
+    })
 
-// Captura la secccion llamada "crearOVerNotas"
-let seccioncrearOVerNotas = document.getElementsByClassName("ocultar")[0]
-console.log(seccioncrearOVerNotas)
-// Enventos
-inicioBotonComenzar.addEventListener("click", mostrarCrearOVerNota)
+    let seccionNotas = document.getElementById("verNotas--contenido");
+    let contenidoNotas = ""
+    for (const nota of notasFiltradas) {
+        contenidoNotas += `
+            <div class="nota" id=${nota.id}>
+              <div class="contenido__nota">
+                <h1>${nota.titulo}</h1>
+                <p>${nota.parrafo}</p>
+              </div>
+            </div>
+        `
+    }
+    seccionNotas.innerHTML = contenidoNotas
+}
+function filtrarAlfabeticamente(notas) {
+    notas.sort((a, b) => {
+        const tituloA = a.titulo.toLowerCase()
+        const tituloB = b.titulo.toLowerCase()
+        if (tituloA < tituloB) {
+            return -1
+        }
+        if (tituloA > tituloB) {
+            return 1
+        }
+        return 0
+    })
 
-function mostrarCrearOVerNota() {
-    alert("Hola esta funcionando la función mostrarCrearOVerNota")
-    // Elimina id de la seccion inicio
-    seccionInicio.removeAttribute("id")
-    // Agrega la clase "ocultar"
-    seccionInicio.className = "ocultar"
-
-    // Elimina la clase ocultar de la seccion CrearOVerNota y en este punto está visible
-    seccioncrearOVerNotas.classList.remove("ocultar")
-    alert("La seccion mostrarOVerNota está visible")
-    // Agrega la clase "ocultar"
-    seccioncrearOVerNotas.className = "crearOVerNotas"
+    let seccionNotas = document.getElementById("verNotas--contenido")
+    let contenidoNotas = ""
+    for (const nota of notas) {
+        contenidoNotas += `
+            <div class="nota" id=${nota.id}>
+              <div class="contenido__nota">
+                <h1>${nota.titulo}</h1>
+                <p>${nota.parrafo}</p>
+              </div>
+            </div>
+        `
+    }
+    seccionNotas.innerHTML = contenidoNotas
 }
 
-// Captura elemento con id "boton__regresar"
-let mostrarOVerNotaBotonRegresar = document.getElementById("boton__regresar")
-// Evento
-mostrarOVerNotaBotonRegresar.addEventListener("click", regresarAInicio)
-
-function regresarAInicio() {
-    // Remover id y poner la clase "ocultar" al nodo crearOVerNotas
-    seccioncrearOVerNotas.removeAttribute("id")
-    seccioncrearOVerNotas.className ="ocultar"
-     // Remover clase y poner el id "inicio" al nodo ocultar(inicio)
-    seccionInicio.classList.remove("ocultar")
-    seccionInicio.id = "inicio"
+function ocultarNodos(nodo) {
+    nodo.removeAttribute("id")
+    nodo.className = "ocultar"
 }
-
-// Captura elemento con id "botonCrear"
-let mostrarOVerNotaBotonCrear = document.getElementById("botonCrear")
-// Evento
-mostrarOVerNotaBotonCrear.addEventListener("click", mostrarCrearNota)
-
-// Captura elemento con la clase "ocultar"
-let seccionCrearNotas = document.getElementsByClassName("ocultar")[1]
-console.log(seccionCrearNotas)
-
-function mostrarCrearNota () {
-    alert("Hola esta funcionando la función mostrarCrearNota")
-    // Elimina la clase "ocultar" de la seccion crearNota
-    seccionCrearNotas.classList.remove("ocultar")
-    // Agrega el id "crearNota"
-    seccionCrearNotas.id = "crearNota"
-
-    // Elimina la clase CrearOVerNota de la seccion crearOVerNota y en este punto está invisible
-    seccioncrearOVerNotas.classList.remove("crearOVerNotas")
-    alert("La seccion mostrarOVerNota está invisible")
-    // Agrega la clase "ocultar"
-    seccioncrearOVerNotas.className = "ocultar"
+function mostrarNodos(nodo, nombreID) {
+    nodo.removeAttribute("class")
+    nodo.id = nombreID
 }
-
